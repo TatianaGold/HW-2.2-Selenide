bd2d04da2c20178fd78e91fa0bd9ab0e6033653e
TatGold <ta-nit@yandex.ru>
2023-05-13 19:11:48 +0300

    Change CardTest.java

--- src/test/java/ru/netology/CardTest.java
+++ src/test/java/ru/netology/CardTest.java
@@ -1,8 +1,10 @@
 package ru.netology;
 
+import com.codeborne.selenide.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -15,20 +17,18 @@ import static com.codeborne.selenide.Selenide.*;
 public class CardTest {
     @BeforeEach
     public void openPage() {
-        WebDriverManager.chromedriver().setup();
-        webdriver().driver().browser().isChrome();
         open("http://localhost:9999/");
     }
 
-    @AfterEach
-    public void endTest() {
-        closeWebDriver();
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
-
     @Test
     void shouldTestCorrectForm() {
         $x("//input[@placeholder=\"Город\"]").val("Майкоп");
-        String planingDate = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[placeholder='Дата встречи']").doubleClick();
+        $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.BACK_SPACE,Keys.HOME,Keys.SHIFT));
+        String planingDate = generateDate(4, "dd.MM.yyyy");
         $x("//input[@placeholder=\"Дата встречи\"]").val(planingDate);
         $("[data-test-id='name'] input").val("Крылова Ольга");
         $("[data-test-id='phone'] input").val("+79883112223");
